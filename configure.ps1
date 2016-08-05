625f115fdcdd69341d068e83282e6f092e89723e
Daniel Fonseca Alves <dalraf@gmail.com>
2016-08-05 16:56:28 -0300

    Adicionado comentários dos parâmetros

--- configure.ps1
+++ configure.ps1
@@ -1,20 +1,34 @@
+# Executar a adição ao domínio, coloque $false caso não deseje a execução
 $addtodomain=$true
 
+# Setar password e ativa o usuário administrador, coloque $false caso não deseje a execução
 $setadminpassword=$true
 
+# Lista de fabricantes de programas que devem ser removidos, exemplo "kaspersky" "microsoft"
 $programsremove = @("mcafee", "avast")
 
+# Lista de fabricantes de programas que devem ser instalados de acordo com a galeria do chocolateycache
+# https://chocolatey.org/packages
 $programsadd = @("googlechrome", "winrar", "spark" , "spark", "flashplayerplugin", "firefox", "adobeair", "cutepdf" ,"javaruntime", "teamviewer -version 10.0.47484", "ultravnc" ,"winscp", "adobereader")
 
+# Diretório de cache do chocolatey aonde seram baixados os executáveis de instalação
+# Esse diretório pode ser copiado para outra cpu após uma instalação para acelerar a instalação.
+$chocolateycache="c:\chocolateycache"
+
+# Password do administador local da cpu
 $passwordadministradorlocal=""
 
+# Domínio AD qualificado aonde a cpu será instalada
 $domain = "example.local"
 
-$passworddomain = "" | ConvertTo-SecureString -asPlainText -Force
-
+# Usuario com permissão para adicionar no domínio
 $usernamedomain = "example\administrador"
 
-$chocolateycache="c:\chocolateycache"
+# Password do usuário com permissão para adicionar no domínio
+$passworddomain = "" | ConvertTo-SecureString -asPlainText -Force
+
+
+
 
 
 foreach($programremove in $programsremove){
